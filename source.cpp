15874964c73cba09345aaeff078a8826116d72e1
gorodilina <104147754+gorodilina@users.noreply.github.com>
2022-04-26 03:39:26 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,1 +1,21 @@
-hjvbnm
+#include "header.h"
+
+using namespace std;
+
+int knapsack (vector<item>& it, int K)
+{
+    int N=it.size();
+    vector<vector<int>> w(K+1, vector<int>(N+1));
+
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<K; j++)
+        {
+            if (it[i].weight<=j+1)
+                w[j+1][i+1]=max(w[j+1][i], w[j+1-it[i].weight][i]+it[i].value);
+            else
+                w[j+1][i+1]=w[j+1][i];
+        }
+    }
+    return w[K][N];
+}
