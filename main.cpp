df2c8b5429e28736494939fcca490b756a880800
gorodilina <104147754+gorodilina@users.noreply.github.com>
2022-04-26 03:37:49 +0300

    the last version

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-hfdjhdkh
+#include "header.h"
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    vector<item> it(N);
+    for (int i=0; i< N; i++)
+        cin >> it[i].value >> it[i].weight;
+
+    int max_value=knapsack (it, K);
+    cout << max_value;
+    return 0;
+}
